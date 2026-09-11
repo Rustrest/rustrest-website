546742d9bbf49b9fd16687e90662669d16c7212f
MuhammadTopu <hossainarman953@gmail.com>
2026-09-11 22:47:23 +0600

    feat: update DocsIndexPage to redirect to external documentation URL

--- app/(marketing)/docs/page.tsx
+++ app/(marketing)/docs/page.tsx
@@ -1,15 +1,18 @@
-import { getSiteSettings } from "@/lib/db/settings";
+import { redirect } from "next/navigation";
+// import { getSiteSettings } from "@/lib/db/settings";
 
 export const dynamic = "force-dynamic";
 
 export default async function DocsIndexPage() {
-  const { docsUrl } = await getSiteSettings();
+  // const { docsUrl } = await getSiteSettings();
+  //
+  // return (
+  //   <iframe
+  //     src={docsUrl}
+  //     title="Documentation"
+  //     className="h-[calc(100dvh-4rem)] w-full border-0"
+  //   />
+  // );
 
-  return (
-    <iframe
-      src={docsUrl}
-      title="Documentation"
-      className="h-[calc(100dvh-4rem)] w-full border-0"
-    />
-  );
+  redirect(process.env.DOCS_URI!);
 }
